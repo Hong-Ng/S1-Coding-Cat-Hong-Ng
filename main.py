a36c2395d53b17cd7c94b2256529f5acf74f4811
Hong-Ng <162881813+Hong-Ng@users.noreply.github.com>
2024-03-12 20:56:50 +0800

    8:52 PM 12/03/2024
    I have fulfilled the task of using minimal variables by using dictionaries. I used the dictionaries to store the information about the interest rate, principal amount and compounding period for Simple and Compound interest. I also made the display easier to read

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-def IntrestCalc(pa,ir,SIcpt,CIcpt,pt,ptu):
-    SI = pa + (((pa*ir*pt)/100)*(pt*SIcpt))
-    SI = round(SI,2)
+def CompoundInterestCalc(pa,ir,cpt,pt,ptu):
 
-    CI = pa*(1+(ir/(100*CIcpt)))**(CIcpt*pt)
-    CI = round(CI,2)
-    return SI, CI
+    amount = pa*(1+(ir/(100*cpt)))**(cpt*pt)
+    amount = round(amount,2)
+    return amount
+
+def SimpleInterestCalc(pa,ir,cpt,pt,ptu):
+    
+    amount = pa + (((pa*(ir/cpt)*(pt*cpt))/100))
+    amount = round(amount,2)
+    return amount
+
+SimpleInterest = {
+    
+}
+
+CompoundInterest = {
+    
+}
 
 TimePeriodYr = {
     'year': 1,
@@ -23,27 +35,37 @@ TimePeriodsMth = {
 print('-----Simple Interest-----')
 pa = float(input("Enter the principal amount in $: "))
 ir = float(input('Enter the interest rate (enter 7% as 7): '))
-SIcpt = input('Enter the intrest rate time unit (Year, Quater, Month, Week, Day): ')
-SIcpt = SIcpt.lower()
-SIcpt = TimePeriodYr[SIcpt]
-print (SIcpt)
+cpt = input('Enter the intrest rate time unit (Year, Quarter, Month, Week, Day): ')
+cpt = cpt.lower()
+cpt = TimePeriodYr[cpt]
 
-SimpleIntrest = IntrestCalc(pa,ir,SIcpt,CIcpt,pt,ptu)
-
+SimpleInterest["principal"] = pa
+SimpleInterest["interest"] = ir
+SimpleInterest['irt'] = cpt
 #Compound interest variables
 print('-----Compound Interest-----')
 pa = float(input("Enter the principal amount in $: "))
 ir = float(input('Enter the interest rate (enter 7% as 7): '))
-CIcpt = input('Enter the intrest rate time unit (Year, Quater, Month, Week, Day): ')
-CIcpt = CIcpt.lower()
-CIcpt = TimePeriodYr[CIcpt]
-print (CIcpt)
+cpt = input('Enter the intrest rate time unit (Year, Quarter, Month, Week, Day): ')
+cpt = cpt.lower()
+cpt = TimePeriodYr[cpt]
 
-CompoundIntrest = IntrestCalc(pa,ir,SIcpt,CIcpt,pt,ptu)
+CompoundInterest["principal"] = pa
+CompoundInterest["interest"] = ir
+CompoundInterest['irt'] = cpt
 
 print("-----Future Projection Timeframes-----")
 pt = int(input('Enter the amount of time to project into the future: '))
 ptu = 'year'
 
+SimpleInterest['amount'] = SimpleInterestCalc(SimpleInterest['principal'],SimpleInterest['interest'],SimpleInterest['irt'],pt,ptu)
 
-print(f'After {pt} {ptu} your Simple Interest account is at ${SimpleIntrest} and your Compound Interest account is at ${CompoundIntrest}')
+CompoundInterest['amount'] = CompoundInterestCalc(CompoundInterest['principal'],CompoundInterest['interest'],CompoundInterest['irt'],pt,ptu)
+
+
+print("========================================================= Perth Modern School Bank =========================================================")       
+
+
+print(f'----- {pt} {ptu}s later ------')
+print (f'Simple Interest Account Balance: {SimpleInterest["amount"]}')
+print(f'Compound Interest Account Balance: {CompoundInterest["amount"]}')
