4693e398928ccbf280532428b941f0595f0fed35
Hong-Ng <162881813+Hong-Ng@users.noreply.github.com>
2024-03-14 21:05:31 +0800

    9:01 PM 14/03/2024
    I have successfully tackled the overflow error by creating a path back to a planned menu. I also condensed the code into multiple functions

--- main.py
+++ main.py
@@ -11,6 +11,78 @@ def SimpleInterestCalc(pa,ir,cpt,pt,ptu):
     amount = round(amount,2)
     return amount
 
+def Mod1():
+    print('==========Module 1==========')
+    print('-----Simple Interest-----')
+    pa = float(input("Enter the principal amount in $: "))
+    ir = float(input('Enter the interest rate (enter 7% as 7): '))
+    cpt = input('Enter the intrest rate time unit (Year, Quarter, Month, Week, Day): ')
+    cpt = cpt.lower()
+    cpt = TimePeriodYr[cpt]
+
+    SimpleInterest["principal"] = pa
+    SimpleInterest["interest"] = ir
+    SimpleInterest['irt'] = cpt
+    #Compound interest variables
+    print('-----Compound Interest-----')
+    pa = float(input("Enter the principal amount in $: "))
+    ir = float(input('Enter the interest rate (enter 7% as 7): '))
+    cpt = input('Enter the intrest rate time unit (Year, Quarter, Month, Week, Day, Custom): ')
+    cpt = cpt.lower()
+    if cpt == 'custom':
+        cpt = int(input('Enter the number of compounding time periods per chosen time unit: '))
+    else:
+        cpt = TimePeriodYr[cpt]
+
+    CompoundInterest["principal"] = pa
+    CompoundInterest["interest"] = ir
+    CompoundInterest['irt'] = cpt
+
+    print("-----Future Projection Timeframes-----")
+    pt = int(input('Enter the amount of time to project into the future: '))
+    ptu = 'year'
+    try:
+        SimpleInterest['amount'] = SimpleInterestCalc(SimpleInterest['principal'],SimpleInterest['interest'],SimpleInterest['irt'],pt,ptu)
+    except OverflowError:
+        print('An Overflow Error has occured. You will be sent back to the home screen, please try changing the inputs to ensure this does not happen again.')
+        print('Please feel free to feedback by calling 000 and telling them the code word "Im dying".')
+        
+    try:
+        CompoundInterest['amount'] = CompoundInterestCalc(CompoundInterest['principal'],CompoundInterest['interest'],CompoundInterest['irt'],pt,ptu)
+    except OverflowError:
+        print('An Overflow Error has occured. You will be sent back to the home screen, please try changing the inputs to ensure this does not happen again.')
+        print('Please feel free to feedback by calling 000 and telling them the code word "Im dying".')
+
+
+    print("========================================================= Perth Modern School Bank =========================================================")       
+
+
+    print(f'----- {pt} {ptu}s later ------')
+    print (f'Simple Interest Account Balance: {SimpleInterest["amount"]}')
+    print(f'Compound Interest Account Balance: {CompoundInterest["amount"]}')
+
+def Mod2():
+    print("=====Module 2=====")
+    pa = float(input("Enter the principal amount in $: "))
+    ir = float(input('Enter the interest rate (enter 7% as 7): '))
+    cpt = input('Enter the intrest rate time unit (Year, Quarter, Month, Week, Day, Custom): ')
+    cpt = cpt.lower()
+    cpt = TimePeriodYr[cpt]
+    ta = float(input('Please enter your target amount: '))
+    CompoundInterest["principal"] = pa
+    CompoundInterest["interest"] = ir
+    CompoundInterest['irt'] = cpt
+    year = 1
+    while year <= ta:
+        try:
+            CompoundInterest['amount'] = round(CompoundInterestCalc(CompoundInterest['principal'],CompoundInterest['interest'],CompoundInterest['irt'],year,ptu),2)
+            year = year + 1
+        except OverflowError:
+            print('An Overflow Error has occured. You will be sent back to the home screen, please try changing the inputs to ensure this does not happen again.')
+            print('Please feel free to feedback by calling 000 and telling them the code word "Im dying".')
+            
+    print(f"After {year} year(s) you're account has reached ${CompoundInterest['amount']}") 
+
 SimpleInterest = {
     
 }
@@ -32,62 +104,3 @@ TimePeriodsMth = {
     'day': 30
 }    
 
-# simple interest variables
-print('-----Simple Interest-----')
-pa = float(input("Enter the principal amount in $: "))
-ir = float(input('Enter the interest rate (enter 7% as 7): '))
-cpt = input('Enter the intrest rate time unit (Year, Quarter, Month, Week, Day): ')
-cpt = cpt.lower()
-cpt = TimePeriodYr[cpt]
-
-SimpleInterest["principal"] = pa
-SimpleInterest["interest"] = ir
-SimpleInterest['irt'] = cpt
-#Compound interest variables
-print('-----Compound Interest-----')
-pa = float(input("Enter the principal amount in $: "))
-ir = float(input('Enter the interest rate (enter 7% as 7): '))
-cpt = input('Enter the intrest rate time unit (Year, Quarter, Month, Week, Day, Custom): ')
-cpt = cpt.lower()
-if cpt == 'custom':
-    cpt = int(input('Enter the number of compounding time periods per chosen time unit: '))
-else:
-    cpt = TimePeriodYr[cpt]
-
-CompoundInterest["principal"] = pa
-CompoundInterest["interest"] = ir
-CompoundInterest['irt'] = cpt
-
-print("-----Future Projection Timeframes-----")
-pt = int(input('Enter the amount of time to project into the future: '))
-ptu = 'year'
-
-SimpleInterest['amount'] = SimpleInterestCalc(SimpleInterest['principal'],SimpleInterest['interest'],SimpleInterest['irt'],pt,ptu)
-
-CompoundInterest['amount'] = CompoundInterestCalc(CompoundInterest['principal'],CompoundInterest['interest'],CompoundInterest['irt'],pt,ptu)
-
-
-print("========================================================= Perth Modern School Bank =========================================================")       
-
-
-print(f'----- {pt} {ptu}s later ------')
-print (f'Simple Interest Account Balance: {SimpleInterest["amount"]}')
-print(f'Compound Interest Account Balance: {CompoundInterest["amount"]}')
-
-
-print("=====Module 2=====")
-pa = float(input("Enter the principal amount in $: "))
-ir = float(input('Enter the interest rate (enter 7% as 7): '))
-cpt = input('Enter the intrest rate time unit (Year, Quarter, Month, Week, Day, Custom): ')
-cpt = cpt.lower()
-cpt = TimePeriodYr[cpt]
-ta = float(input('Please enter your target amount: '))
-CompoundInterest["principal"] = pa
-CompoundInterest["interest"] = ir
-CompoundInterest['irt'] = cpt
-year = 1
-while year <= ta:
-    CompoundInterest['amount'] = round(CompoundInterestCalc(CompoundInterest['principal'],CompoundInterest['interest'],CompoundInterest['irt'],year,ptu),2)
-    year = year + 1
-
-print(f"After {year} year(s) you're account has reached ${CompoundInterest['amount']}")
