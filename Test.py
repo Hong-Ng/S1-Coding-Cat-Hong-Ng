8c923f127916cb6c4b06bceb12e1eb848427793f
Hong-Ng <162881813+Hong-Ng@users.noreply.github.com>
2024-03-18 21:05:24 +0800

    18/3/2024 9:03PM
    Slight updates to the previously committed code, nothing significant.

--- Test.py
+++ Test.py
@@ -1,1 +1,16 @@
-import this
+TimePeriod = {
+    'year': 365,
+    'quarter': 91.25,
+    'month': 30.4167,
+    'week': 7.0192,
+    'day': 1
+}
+i = 5
+it = TimePeriod['year']
+i = 5/(TimePeriod['year']/TimePeriod['quarter'])
+kt = TimePeriod['quarter']
+k = TimePeriod['month']/kt
+t = 5
+p = 100
+
+A = 100*(1+(i/k)**kt*k)
